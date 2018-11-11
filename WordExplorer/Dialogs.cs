d88643155282371fa5e15197c816b72cbc484efa
zhouxindong <zhouxindong@163.com>
2018-11-11 09:06:59 +0800

    add helper class

---
+++ WordExplorer/Dialogs.cs
@@ -0,0 +1,130 @@
+﻿using System.IO;
+using System.Windows.Forms;
+
+namespace WordExplorer
+{
+    public class Dialogs
+    {
+        /// <summary>
+        /// This class is purely static.
+        /// </summary>
+        private Dialogs() { }
+
+        /// <summary>
+        /// Stores the last accessed file path, so that the next file dialog session should start from there.
+        /// </summary>
+        private static string gDocumentPath = Application.StartupPath;
+        /// <summary>
+        /// Stores the name of the currently loaded dopcument. Name is stored without extension to be used when saving in different file format.
+        /// </summary>
+        private static string gDocumentName;
+
+        /// <summary>
+        /// Selects file name for document to open.
+        /// </summary>
+        /// <returns>File name of the document selected to be opened.</returns>
+        public static string OpenDocument()
+        {
+            return OpenDocument(Globals.OpenFileFilter);
+        }
+
+        /// <summary>
+        /// Selects AsposePdf XML file name for document to open.
+        /// </summary>
+        /// <returns>File name of the document selected to be opened.</returns>
+        public static string OpenAsposePdfDocument()
+        {
+            return OpenDocument(Globals.OpenAsposePdfFileFilter);
+        }
+
+        /// <summary>
+        /// Selects file name for document to open.
+        /// </summary>
+        /// <param name="filter">File name filter.</param>
+        /// <returns>File name of the document selected to be opened.</returns>
+        public static string OpenDocument(string filter)
+        {
+            // Optimized to allow automatic conversion to VB.NET
+            OpenFileDialog dlg = new OpenFileDialog();
+            try
+            {
+                dlg.CheckFileExists = true;
+                dlg.CheckPathExists = true;
+                dlg.Title = Globals.OpenDocumentDialogTitle;
+                dlg.InitialDirectory = gDocumentPath;
+                dlg.Filter = filter;
+
+                // Optimized to allow automatic conversion to VB.NET
+                DialogResult dlgResult = dlg.ShowDialog(Globals.MainForm);
+                if (dlgResult.Equals(DialogResult.OK))
+                {
+                    gDocumentPath = Path.GetDirectoryName(dlg.FileName);
+                    gDocumentName = Path.GetFileNameWithoutExtension(dlg.FileName);
+                    return dlg.FileName;
+                }
+                else
+                {
+                    return string.Empty;
+                }
+            }
+            finally
+            {
+                dlg.Dispose();
+            }
+        }
+
+        /// <summary>
+        /// Selects file name for saving currently opened document.
+        /// </summary>
+        /// <returns>File name to save the currently chosen document with.</returns>
+        public static string SaveDocument()
+        {
+            return SaveDocument(Globals.SaveFileFilter);
+        }
+
+        /// <summary>
+        /// Selects PDF file name for saving currently opened document.
+        /// </summary>
+        /// <returns>File name to save the currently chosen document with.</returns>
+        public static string SavePdfDocument()
+        {
+            return SaveDocument(Globals.SavePdfFileFilter);
+        }
+
+        /// <summary>
+        /// Selects file name for saving currently opened document.
+        /// </summary>
+        /// <param name="filter">File name filter.</param>
+        /// <returns>File name to save the currently chosen document with.</returns>
+        public static string SaveDocument(string filter)
+        {
+            // Optimized to allow automatic conversion to VB.NET
+            SaveFileDialog dlg = new SaveFileDialog();
+            try
+            {
+                dlg.CheckFileExists = false;
+                dlg.CheckPathExists = true;
+                dlg.Title = Globals.SaveDocumentDialogTitle;
+                dlg.InitialDirectory = gDocumentPath;
+                dlg.Filter = filter;
+                dlg.FileName = gDocumentName;
+
+                // Optimized to allow automatic conversion to VB.NET
+                DialogResult dlgResult = dlg.ShowDialog(Globals.MainForm);
+                if (dlgResult.Equals(DialogResult.OK))
+                {
+                    gDocumentPath = Path.GetDirectoryName(dlg.FileName);
+                    return dlg.FileName;
+                }
+                else
+                {
+                    return string.Empty;
+                }
+            }
+            finally
+            {
+                dlg.Dispose();
+            }
+        }
+    }
+}
---
+++ WordExplorer/Global.cs
@@ -0,0 +1,54 @@
+﻿using Aspose.Words;
+
+namespace WordExplorer
+{
+    public class Globals
+    {
+        /// <summary>
+        /// This class is purely static, that's why we prevent instance creation by declaring instance constructor as private.
+        /// </summary>
+        private Globals() { }
+
+        // Titles
+        internal static string ApplicationTitle = "Document Explorer";
+        internal static readonly string UnexpectedExceptionDialogTitle = ApplicationTitle + " - unexpected error occured";
+        internal static readonly string OpenDocumentDialogTitle = "Open Document";
+        internal static readonly string SaveDocumentDialogTitle = "Save Document As";
+
+        // File filters
+        internal static readonly string OpenFileFilter =
+            "All Word Documents (*.docx;*.dotx;*.doc;*.dot;*.htm;*.html;*.rtf;*.xml;*.wml)|*.docx;*.dotx;*.doc;*.dot;*.htm;*.html;*.rtf;*.xml;*.wml|" +
+            "Word Documents (*.docx;*.dotx;*.doc;*.dot)|*.docx;*.dotx;*.doc;*.dot|" +
+            "All Web Pages (*.htm;*.html)|*.htm;*.html|" +
+            "Rich Text Format (*.rtf)|*.rtf|" +
+            "XML Files (*.xml;*.wml)|*.xml;*.wml|" +
+            "All Files (*.*)|*.*";
+
+        internal static readonly string OpenAsposePdfFileFilter =
+            "Aspose.Pdf XML (*.AsposePdf)|*.AsposePdf";
+
+        internal static readonly string SaveFileFilter =
+            "Microsoft Word 97 - 2003 Document (*.doc)|*.doc|" +
+            "Microsoft Office 2007 Open XML (*.docx)|*.docx|" +
+            "Adobe Acrobat Document (*.pdf)|*.pdf|" +
+            "Rich Text Format (*.rtf)|*.rtf|" +
+            "Microsoft Word 2003 WordprocessingML (*.xml)|*.xml|" +
+            "Web Page (*.html)|*.html|" +
+            "Plain Text (*.txt)|*.txt|" +
+            "Aspose.Pdf XML (*.AsposePdf)|*.AsposePdf";
+
+        internal static readonly string SavePdfFileFilter =
+            "Adobe Acrobat Document (*.pdf)|*.pdf";
+
+        /// <summary>
+        /// Reference for application's main form.
+        /// </summary>
+        internal static MainForm MainForm;
+
+        /// <summary>
+        /// Reference for currently loaded 鈕cument.
+        /// </summary>
+        internal static Document Document;
+    }
+
+}
